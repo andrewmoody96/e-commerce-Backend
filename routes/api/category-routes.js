47611fc3418bba9fa53ca7bba9e3441c362d8bc1
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-29 12:31:45 -0500

    Added sequelize to server.js -- Commented out additional routes in routes for now.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product, Tag, ProductTag } = require('../../models');
 
 // The `/api/categories` endpoint
 
@@ -16,21 +16,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+// router.get('/:id', (req, res) => {
+//   // find one category by its `id` value
+//   // be sure to include its associated Products
+// });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
+// router.post('/', (req, res) => {
+//   // create a new category
+// });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+// router.put('/:id', (req, res) => {
+//   // update a category by its `id` value
+// });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+// router.delete('/:id', (req, res) => {
+//   // delete a category by its `id` value
+// });
 
 module.exports = router;
